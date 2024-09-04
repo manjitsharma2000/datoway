e4ff98d7bf70cc7dde32b3554ee4789ce5e8ad6c
manjitsharma2000 <manjeet.designs@gmail.com>
2024-09-04 19:16:45 +0530

    deploy

---
+++ app/separator.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Image from "next/image";
+
+import separatorTop from "../public/images/separator/separator-top.svg";
+import separatorBottom from "../public/images/separator/separator-bottom.svg";
+import separatorTopLight from "../public/images/light/separator/separator-top.svg";
+import separatorBottomLight from "../public/images/light/separator/separator-bottom.svg";
+
+const Separator = ({ top, isLightTheme }) => {
+  return (
+    <>
+      {top ? (
+        <div className="chatenai-separator">
+          {isLightTheme ? (
+            <Image
+              className={"w-100 separator-dark"}
+              src={separatorTop}
+              alt="separator"
+            />
+          ) : (
+            <Image
+              className={"w-100 separator-light"}
+              src={separatorTopLight}
+              alt="separator"
+            />
+          )}
+        </div>
+      ) : (
+        <div className="chatenai-separator">
+          {isLightTheme ? (
+            <Image
+              className={"w-100 separator-dark"}
+              src={separatorBottom}
+              alt="separator"
+            />
+          ) : (
+            <Image
+              className={"w-100 separator-light"}
+              src={separatorBottomLight}
+              alt="separator"
+            />
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Separator;
