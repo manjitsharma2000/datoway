52dba4b4e48503998e77040f376aa8b32418711a
manjitsharma2000 <160587463+manjitsharma2000@users.noreply.github.com>
2024-09-10 21:11:46 +0530

    Update Nav.js

--- components/Header/Nav.js
+++ components/Header/Nav.js
@@ -20,10 +20,50 @@ const Nav = () => {
   return (
     <>
       <ul className="mainmenu">
-        <li>
+       /* <li>
           <Link href="/dashboard">Why Datoway</Link>
-        </li>
+        </li>*/
         <li className="with-megamenu has-menu-child-item position-relative">
+          <a
+            href="#"
+            onClick={() => setShowItem(!showItem)}
+            className={`${!showItem ? "open" : ""}`}
+          >
+            Why Datoway
+          </a>
+          <div
+            className={`rainbow-megamenu right-align with-mega-item-2 ${
+              showItem ? "" : "d-block"
+            }`}
+          >
+            <div className="wrapper p-0">
+              <div className="row row--0">
+                <div className="col-lg-6 single-mega-item">
+                  <h3 className="rbt-short-title">DASHBOARD PAGES</h3>
+                  <ul className="mega-menu-item">
+                    {DashboardItem &&
+                      DashboardItem.navDashboardItem.map((data, index) => (
+                        <li key={index}>
+                          <Link
+                            href={data.link}
+                            className={isActive(data.link) ? "active" : ""}
+                          >
+                            {data.text}
+                          </Link>
+                        </li>
+                      ))}
+                  </ul>
+                </div>
+                <div className="col-lg-6 single-mega-item">
+                  <div className="header-menu-img">
+                    <Image src={menuImg} alt="Menu Split Image" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </li>
+ <li className="with-megamenu has-menu-child-item position-relative">
           <a
             href="#"
             onClick={() => setShowItem(!showItem)}
@@ -98,11 +138,78 @@ const Nav = () => {
             </div>
           </div>
         </li>
-        <li>
-          <Link href="/pricing">Services</Link>
+                         <li className="with-megamenu has-menu-child-item position-relative">
+          <a
+            href="#"
+            onClick={() => setShowPage(!showPage)}
+            className={`${!showPage ? "open" : ""}`}
+          >
+            Services
+          </a>
+          <div
+            className={`rainbow-megamenu right-align with-mega-item-2 small ${
+              showPage ? "" : "d-block"
+            }`}
+          >
+            <div className="wrapper p-0">
+              <div className="row row--0">
+                <div className="col-lg-12 single-mega-item">
+                  <h3 className="rbt-short-title">Inner Pages</h3>
+                  <ul className="mega-menu-item">
+                    {DashboardItem &&
+                      DashboardItem.navPageItem.map((data, index) => (
+                        <li key={index}>
+                          <Link
+                            href={data.link}
+                            className={isActive(data.link) ? "active" : ""}
+                          >
+                            {data.text}
+                          </Link>
+                        </li>
+                      ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
         </li>
-                          <li>
-          <Link href="/pricing">Abou</Link>
+      /*  <li>
+          <Link href="/pricing">Services</Link>
+        </li>*/
+                          <li className="with-megamenu has-menu-child-item position-relative">
+          <a
+            href="#"
+            onClick={() => setShowPage(!showPage)}
+            className={`${!showPage ? "open" : ""}`}
+          >
+            About
+          </a>
+          <div
+            className={`rainbow-megamenu right-align with-mega-item-2 small ${
+              showPage ? "" : "d-block"
+            }`}
+          >
+            <div className="wrapper p-0">
+              <div className="row row--0">
+                <div className="col-lg-12 single-mega-item">
+                  <h3 className="rbt-short-title">Inner Pages</h3>
+                  <ul className="mega-menu-item">
+                    {DashboardItem &&
+                      DashboardItem.navPageItem.map((data, index) => (
+                        <li key={index}>
+                          <Link
+                            href={data.link}
+                            className={isActive(data.link) ? "active" : ""}
+                          >
+                            {data.text}
+                          </Link>
+                        </li>
+                      ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
         </li>
         <li>
           <Link href="/signin">Sign In</Link>
