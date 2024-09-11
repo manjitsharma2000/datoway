2700d522d40f2157296d0af21e9ead98212c0062
manjitsharma2000 <160587463+manjitsharma2000@users.noreply.github.com>
2024-09-11 13:51:55 +0530

    Update Service.js

--- components/Service/Service.js
+++ components/Service/Service.js
@@ -36,13 +36,13 @@ const Service = () => {
                 data-sal-delay="100"
               >
                 <h4 className="subtitle">
-                  <span className="theme-gradient">GET IN TOUCH FOR FREE</span>
+                  <span className="theme-gradient">DATOWAY</span>
                 </h4>
                 <h2 className="title w-600 mb--20">
-                  Instant Content Generation with AI
+                 Portfolio
                 </h2>
                 <p className="description b1">
-                  Provide Descriptions, Get Instant AI-Generated Content
+                  Explore the future of innovation with Datoway's AI-powered software products and solutions, carefully designed to leverage the latest technologies. Stay ahead of the competition as we equip you with transformative tools like Generative AI, LLM, and Machine Learning to tackle challenges and unlock new opportunities. Discover how our cutting-edge solutions can revolutionize your business by diving into our portfolio, where speed, quality, and pioneering spirit drive every innovation.
                 </p>
               </div>
             </div>
