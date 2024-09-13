19dbe728822a6fb8d0371ad385873e1c6e7d9664
manjitsharma2000 <160587463+manjitsharma2000@users.noreply.github.com>
2024-09-13 13:36:29 +0530

    Update Nav.js

--- components/Header/Nav.js
+++ components/Header/Nav.js
@@ -155,7 +155,7 @@ const Nav = () => {
                   <h3 className="rbt-short-title">Inner Pages</h3>
                   <ul className="mega-menu-item">
                     {DashboardItem &&
-                      DashboardItem.navPageItem.map((data, index) => (
+                      DashboardItem.navServicesItem.map((data, index) => (
                         <li key={index}>
                           <Link
                             href={data.link}
