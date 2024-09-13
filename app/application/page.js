c9992b42d0a051f45b71a092d55c0b9758d7fa71
manjitsharma2000 <160587463+manjitsharma2000@users.noreply.github.com>
2024-09-13 16:26:10 +0530

    Update page.js

--- app/application/page.js
+++ app/application/page.js
@@ -1,8 +1,8 @@
 import ApplicationPage from "./index";
 
 export const metadata = {
-  title: "Applications - Datoway",
-  description: "Datoway - Reliable Transformation with Data, Insights and Speed",
+  title: "Applications - AI SaaS Website NEXTJS14 UI Kit",
+  description: "ChatenAI - AI SaaS Website NEXTJS14 UI Kit",
 };
 
 const ApplicationLayout = () => {
